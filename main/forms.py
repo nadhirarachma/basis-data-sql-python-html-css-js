b7fda37b2d640b84f9c233c3baf4a4bab7f5b54b
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2022-05-12 19:59:57 +0700

    add page crud produk.

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    email = forms.CharField(label='E-Mail', max_length=50)
+    email = forms.CharField(label='E-mail', max_length=50)
     password = forms.CharField(label='Password', max_length=50)
