8d72f2304c844b48d56b879534bd4fb38c3964a7
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2022-05-25 18:44:14 +0700

    fitur create and update produk

--- cr_histori_pmakanan/views.py
+++ cr_histori_pmakanan/views.py
@@ -1,3 +1,4 @@
+from time import time
 from django.shortcuts import render
 from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.db import connection
@@ -43,11 +44,92 @@ def listHistori(request):
    else:
         return HttpResponseRedirect('/login')
 
-def buatProduk(request):
-    if (request.session['role'] == ['admin']):
-        role = "admin"
+def buatProduksi(request):
+    # if (request.session['role'] == ['admin']):
+    #     role = "admin"
 
-    else:
-        role = "pengguna"
+    # else:
+    #     role = "pengguna"
     
-    return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduk, "role" : role})
+    # return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduk, "role" : role})
+
+    if request.session.has_key('email'):
+        cursor = connection.cursor()
+        result = []
+        try:
+            if (request.session['role'] == ['admin']):
+                role = "admin"
+                return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role})
+
+            else:
+                role = "pengguna"
+                form = BuatProduksi(request.POST, request.FILES)
+                cursor = connection.cursor()
+                result = []
+
+                cursor.execute("SET SEARCH_PATH TO HIDAY")
+                cursor.execute("SELECT id, nama FROM PRODUK")
+                result = tupleFetch(cursor)
+                cursor.execute("SET SEARCH_PATH TO public")
+
+                print(result)
+
+                # context = {
+                #     'id' : result[0],
+                #     'nama' : result[1],
+                #     'hargajual' : result[2],
+                #     'sifat' : result[3],
+                #     'jenisproduk' : result[4]
+                # }
+
+                if (form.is_valid and request.method == 'POST'):
+                    idproduk = form.data.get("IdProduk")
+                    jumlah = form.data.get("Jumlah")
+                    xp = form.data.get("XP")
+
+                    print(idproduk)
+                    print(jumlah)
+                    print(xp)
+
+                    email = request.session['email'][0]
+
+                    cursor.execute("SET SEARCH_PATH TO HIDAY")
+
+                    cursor.execute("SELECT jumlah, id_produk FROM PRODUK_DIBUTUHKAN_OLEH_PRODUK_MAKANAN WHERE id_produk_makanan = '" + idproduk + "'")
+                    result = cursor.fetchone()
+
+                    jumlahdibutuhkan = result[0]
+                    produkdibutuhkan = result[1]
+                    print(jumlahdibutuhkan)
+                    print(produkdibutuhkan)
+
+                    cursor.execute("SELECT jumlah FROM LUMBUNG_MEMILIKI_PRODUK WHERE id_lumbung = '" + email + "' AND id_produk = '" + produkdibutuhkan + "'")
+                    result = cursor.fetchone()
+
+                    jumlahdipengguna = result[0]
+                    print(jumlahdipengguna)
+
+                    cursor.execute("SELECT id_alat_produksi FROM PRODUKSI WHERE id_produk_makanan = '" + idproduk + "'")
+                    result = cursor.fetchone()
+
+                    alatproduksi = result[0]
+                    print(alatproduksi)
+
+                    
+
+                    # if ((jumlahdipengguna > jumlahdibutuhkan * jumlah)):
+                        
+
+                    cursor.execute("INSERT INTO ")
+                    cursor.execute("SET SEARCH_PATH TO public")
+                    cursor.close()
+
+                    return HttpResponseRedirect('/crud-produk/list-produk')
+
+                return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role, "result" : result})
+            
+        except Exception as e:
+            print(e)            
+        
+    else:
+        return HttpResponseRedirect('/login')
