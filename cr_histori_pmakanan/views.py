b85aa0518f888ef8e65bf1f8c9772d5647f34dfd
David Johan Hasiholan Parhusip <david.johan@ui.ac.id>
2022-05-26 02:15:06 +0700

    fitur create histori produk makanan

--- cr_histori_pmakanan/views.py
+++ cr_histori_pmakanan/views.py
@@ -4,6 +4,8 @@ from django.http.response import HttpResponseNotFound, HttpResponseRedirect
 from django.db import connection
 from collections import namedtuple
 from .forms import *
+from datetime import datetime
+from django.contrib import messages
 
 def tupleFetch(cursor):
     desc = cursor.description
@@ -17,7 +19,6 @@ def listHistori(request):
         role =''
         try:
             # cursor.execute("SET SEARCH_PATH TO HIDAY")
-            
             if (request.session['role'] == ['admin']):    
                 cursor.execute("SELECT HP.email, HP.waktu_awal, HP.waktu_selesai, HP.jumlah, HP.xp, P.nama as namaproduk, A.nama as asetalat from hiday.HISTORI_PRODUKSI HP JOIN hiday.HISTORI_PRODUKSI_MAKANAN HPM ON HP.email = HPM.email AND HP.waktu_awal = HPM.waktu_awal JOIN hiday.PRODUK P ON HPM.id_produk_makanan = P.id JOIN hiday.ASET A ON HPM.id_alat_produksi = A.id")
                 result = tupleFetch(cursor)    
@@ -45,14 +46,6 @@ def listHistori(request):
         return HttpResponseRedirect('/login')
 
 def buatProduksi(request):
-    # if (request.session['role'] == ['admin']):
-    #     role = "admin"
-
-    # else:
-    #     role = "pengguna"
-    
-    # return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduk, "role" : role})
-
     if request.session.has_key('email'):
         cursor = connection.cursor()
         result = []
@@ -68,24 +61,16 @@ def buatProduksi(request):
                 result = []
 
                 cursor.execute("SET SEARCH_PATH TO HIDAY")
-                cursor.execute("SELECT id, nama FROM PRODUK")
+                cursor.execute("SELECT id, nama FROM PRODUK WHERE id LIKE'PM%'")
                 result = tupleFetch(cursor)
                 cursor.execute("SET SEARCH_PATH TO public")
 
                 print(result)
 
-                # context = {
-                #     'id' : result[0],
-                #     'nama' : result[1],
-                #     'hargajual' : result[2],
-                #     'sifat' : result[3],
-                #     'jenisproduk' : result[4]
-                # }
-
                 if (form.is_valid and request.method == 'POST'):
                     idproduk = form.data.get("IdProduk")
                     jumlah = form.data.get("Jumlah")
-                    xp = form.data.get("XP")
+                    xp = str(5 * int(jumlah))
 
                     print(idproduk)
                     print(jumlah)
@@ -103,11 +88,20 @@ def buatProduksi(request):
                     print(jumlahdibutuhkan)
                     print(produkdibutuhkan)
 
-                    cursor.execute("SELECT jumlah FROM LUMBUNG_MEMILIKI_PRODUK WHERE id_lumbung = '" + email + "' AND id_produk = '" + produkdibutuhkan + "'")
-                    result = cursor.fetchone()
+                    # cursor.execute("SELECT jumlah FROM LUMBUNG_MEMILIKI_PRODUK WHERE id_lumbung = '" + email + "' AND id_produk = '" + produkdibutuhkan + "'")
+                    cursor.execute("SELECT id_produk, jumlah FROM LUMBUNG_MEMILIKI_PRODUK WHERE id_lumbung = '" + email + "'")
+                    result = tupleFetch(cursor)
 
-                    jumlahdipengguna = result[0]
-                    print(jumlahdipengguna)
+                    checkerSatu = False #ngecek usernya punya produk buat produksi atauga
+                    jumlahdipengguna = 0
+                    for i in range(len(result)):
+                        print(result[i][0])
+                        if (produkdibutuhkan == result[i][0]):
+                            jumlahdipengguna = result[i][1]
+                            checkerSatu = True
+
+                    # jumlahdipengguna = result[0]
+                    # print(jumlahdipengguna)
 
                     cursor.execute("SELECT id_alat_produksi FROM PRODUKSI WHERE id_produk_makanan = '" + idproduk + "'")
                     result = cursor.fetchone()
@@ -115,18 +109,43 @@ def buatProduksi(request):
                     alatproduksi = result[0]
                     print(alatproduksi)
 
-                    
+                    cursor.execute("SELECT id_koleksi_aset FROM KOLEKSI_ASET_MEMILIKI_ASET WHERE id_aset = '" + alatproduksi + "'")
+                    result = tupleFetch(cursor)
+                    print(result)
 
-                    # if ((jumlahdipengguna > jumlahdibutuhkan * jumlah)):
-                        
+                    checkerDua = False #ngecek usernya punya alat(aset) buat produksi atauga
+                    for i in range(len(result)):
+                        print(result[i][0])
+                        if (email == result[i][0]):
+                            checkerDua = True
 
-                    cursor.execute("INSERT INTO ")
+                    waktuAwal = str(datetime.now())
+                    waktuSelesai = str(datetime.now())
+
+                    if (int(jumlahdipengguna) < int(jumlahdibutuhkan) * int(jumlah)):
+                        cursor.execute("SELECT id, nama FROM PRODUK WHERE id LIKE'PM%'")
+                        result = tupleFetch(cursor)
+                        cursor.execute("SET SEARCH_PATH TO public")
+                        cursor.close()
+                        print("GAGALLL")
+                        return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role, "result" : result, "gagal" : "gagal"})
+
+                    if ((int(jumlahdipengguna) >= int(jumlahdibutuhkan) * int(jumlah)) and (checkerSatu) and (checkerDua)):
+                        cursor.execute("INSERT INTO HISTORI_PRODUKSI VALUES ('" + email + "', '" + waktuAwal + "', '" + waktuSelesai + "', '" + jumlah + "' , '" + xp + "')")
+                        cursor.execute("INSERT INTO HISTORI_PRODUKSI_MAKANAN VALUES ('" + email + "', '" + waktuAwal + "', '" + alatproduksi + "', '" + idproduk + "')")
+                        cursor.execute("SET SEARCH_PATH TO public")
+                        cursor.close()
+                        return HttpResponseRedirect('/cr-histori-pmakanan/list-histori-pmakanan')
+
+                    cursor.execute("SELECT id, nama FROM PRODUK WHERE id LIKE'PM%'")
+                    result = tupleFetch(cursor)
                     cursor.execute("SET SEARCH_PATH TO public")
                     cursor.close()
 
-                    return HttpResponseRedirect('/crud-produk/list-produk')
-
-                return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role, "result" : result})
+                    # return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role, "result" : result, "gagal" : "gagaga"})
+                    # return HttpResponseRedirect('/crud-produk/list-produk')
+                
+                return render(request, 'buat_histori_pmakanan.html', {"form" : BuatProduksi, "role" : role, "result" : result, "gagal" : "tidak"})
             
         except Exception as e:
             print(e)            
